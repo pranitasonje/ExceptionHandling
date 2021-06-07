cb14c9317978d40c2c0bb1ac168823a21bc7ca1e
pranitasonje <85480319+pranitasonje@users.noreply.github.com>
2021-06-07 15:11:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number1:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number2");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                int result = num1 / num2;
+                Console.WriteLine("The result is :" + result);
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+
+            finally
+            {
+                Console.WriteLine("Application Completed");
+            }
+        }
+    }
+}
